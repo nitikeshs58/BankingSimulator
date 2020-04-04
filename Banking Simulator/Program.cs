044d00c3c8481aa74afc9e2ed7b4c0648421fa72
nitikeshs58 <nitikeshs58@gmail.com>
2020-04-04 11:43:48 +0530

    Added: Welcome message to banking simulator.

---
+++ Banking Simulator/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Banking_Simulator
+{
+    class Program
+    {
+        // main method
+        static void Main(string[] args)
+        {
+            Console.WriteLine("!!Welcome to Banking Simulator Cash Counter!!");
+        }
+    }
+}
