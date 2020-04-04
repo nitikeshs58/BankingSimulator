7361addfdc1d417eaf4cd8202b96444c4de0d22d
nitikeshs58 <nitikeshs58@gmail.com>
2020-04-04 17:56:45 +0530

    Refactored: to seperate code in different files using linkedList.

---
+++ Banking Simulator/CashCounterMain.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Banking_Simulator
+{
+    class CashCounterMain
+    {
+        // main method
+        static void Main(string[] args)
+        {
+            Console.WriteLine("!!Welcome to Banking Simulator Cash Counter!!");
+            LinkedList link = new LinkedList(); // object creation of linkedlist
+            Banking bank = new Banking(); // object creation of Banking
+
+            int count = 0;
+            int numberOfAccountants = 5;
+            int choice = 0;
+            while (count < numberOfAccountants)
+            {
+                int accountantBalance = 5000;
+                int totalAccountantBalance = 0;
+                try
+                {
+                    Console.WriteLine("Enter your Choice :\nPress 1: toDeposite \nPress 2: toWithdraw");
+                    choice = Convert.ToInt32(Console.ReadLine());
+
+                    switch (choice)
+                    {
+                        case 1:
+                            Console.WriteLine("Enter Deposite amount: ");
+                            int depositeCash = Convert.ToInt32(Console.ReadLine());
+                            totalAccountantBalance = bank.cashDeposite(depositeCash, accountantBalance);
+                            link.insertAtLast(totalAccountantBalance);
+                            link.showLinkedlist();
+                            link.removeAtFirst();
+                            depositeCash = 0;
+                            break;
+
+                        case 2:
+                            Console.WriteLine("Enter withdraw amount: ");
+                            int withdrawCash = Convert.ToInt32(Console.ReadLine());
+                            if (accountantBalance < withdrawCash)
+                            {
+                                Console.WriteLine("!!** insufficient balance to withdraw **!!\n");
+                            }
+                            else
+                            {
+                                totalAccountantBalance = bank.cashWithdraw(withdrawCash, accountantBalance);
+                                link.insertAtLast(totalAccountantBalance);
+                                link.showLinkedlist();
+                                link.removeAtFirst();
+                                withdrawCash = 0;
+                            }
+                            break;
+                    }
+                    count += 1;
+                }
+                catch
+                {
+                    Console.WriteLine("!!**Enter appropriate value.**!!\n");
+                }
+            }
+        }
+    }
+}
--- Banking Simulator/Program.cs
+++ Banking Simulator/Program.cs
@@ -2,25 +2,7 @@
 
 namespace Banking_Simulator
 {
-    // cash cash withdraw and deposite are done
-    public class Banking
-    {
-        // cash withdraw
-        public int cashWithdraw(int cashToWithdraw, int currentBalance)
-        {
-            currentBalance -= cashToWithdraw;
-            return currentBalance;
-        }
-
-        // cash deposite
-        public int cashDeposite(int cashToDeposite, int currentBalance)
-        {
-            currentBalance += cashToDeposite;
-            return currentBalance;
-        }
-    }
-
-    public class Node
+      public class Node
     {
         public int data;// declaring a variable
         public Node next;// referance to next node
@@ -70,65 +52,5 @@ namespace Banking_Simulator
             }
             Console.WriteLine(n.data);
         }
-    }
-
-    class Program
-    {
-        // main method
-        static void Main(string[] args)
-        {
-            Console.WriteLine("!!Welcome to Banking Simulator Cash Counter!!");
-            LinkedList link = new LinkedList(); // object creation of QueueList
-            Banking bank = new Banking(); // object creation of Banking
-
-            int count = 0;
-            int numberOfAccountants = 5;
-            int choice = 0;
-            while (count < numberOfAccountants)
-            {
-                int accountantBalance = 5000;
-                int totalAccountantBalance = 0;
-                try
-                {
-                    Console.WriteLine("Enter your Choice :\nPress 1: toDeposite \nPress 2: toWithdraw");
-                    choice = Convert.ToInt32(Console.ReadLine());
-
-                    switch (choice)
-                    {
-                        case 1:
-                            Console.WriteLine("Enter Deposite amount: ");
-                            int depositeCash = Convert.ToInt32(Console.ReadLine());
-                            totalAccountantBalance = bank.cashDeposite(depositeCash, accountantBalance);
-                            link.insertAtLast(totalAccountantBalance);
-                            link.showLinkedlist();
-                            link.removeAtFirst();
-                            depositeCash = 0;
-                            break;
-
-                        case 2:
-                            Console.WriteLine("Enter withdraw amount: ");
-                            int withdrawCash = Convert.ToInt32(Console.ReadLine());
-                            if (accountantBalance < withdrawCash)
-                            {
-                                Console.WriteLine("!!** insufficient balance to withdraw **!!\n");
-                            }
-                            else
-                            {
-                                totalAccountantBalance = bank.cashWithdraw(withdrawCash, accountantBalance);
-                                link.insertAtLast(totalAccountantBalance);
-                                link.showLinkedlist();
-                                link.removeAtFirst();
-                                withdrawCash = 0;
-                            }
-                            break;
-                    }
-                    count += 1;
-                }
-                catch
-                {
-                    Console.WriteLine("!!**Enter appropriate value.**!!\n");
-                }
-            }
-        }
-    }
+    }    
 }
